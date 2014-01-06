485fecf05972b16c4d2eead59d8fa7e1291b51ed
Benjamin Coe <ben@yesware.com>
2014-01-06 10:53:53 -0800

    Updated rfc2047 gem to work with Ruby 1.9.3+

--- test/rfc2047_test.rb
+++ test/rfc2047_test.rb
@@ -1,7 +1,6 @@
+# encoding: UTF-8
 
-#!/usr/bin/ruby -w
-
-require 'lib/rfc2047'
+require './lib/rfc2047'
 require 'test/unit'
 
 =begin
@@ -50,12 +49,12 @@ class TestVcard < Test::Unit::TestCase
       },
 
       '=?ISO-8859-1?Q?Keld_J=F8rn_Simonsen?= <keld / dkuug.dk>' => {
-        'iso-8859-1' => "Keld J\xF8rn Simonsen <keld / dkuug.dk>",
+        'iso-8859-1' => "Keld J\xF8rn Simonsen <keld / dkuug.dk>".force_encoding("iso-8859-1"),
         'utf-8' => "Keld J\303\270rn Simonsen <keld / dkuug.dk>"
       },
 
       '=?ISO-8859-1?Q?Andr=E9?= Pirard <PIRARD / vm1.ulg.ac.be>' => {
-        'iso-8859-1' => "Andr\xe9 Pirard <PIRARD / vm1.ulg.ac.be>",
+        'iso-8859-1' => "Andr\xe9 Pirard <PIRARD / vm1.ulg.ac.be>".force_encoding("iso-8859-1"),
       },
 
       '=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?= =?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?=' => {
@@ -66,15 +65,15 @@ class TestVcard < Test::Unit::TestCase
         'utf-8' => %(Re: Your tweet: rapportive \342\200\224 "@rapportive still use it every day")
       },
 
-      '=?ISO-8859-1?Q?Olle_J=E4rnefors?= <ojarnef / admin.kth.se>' => {'iso-8859-1' => "Olle J\xE4rnefors <ojarnef / admin.kth.se>",
+      '=?ISO-8859-1?Q?Olle_J=E4rnefors?= <ojarnef / admin.kth.se>' => {'iso-8859-1' => "Olle J\xE4rnefors <ojarnef / admin.kth.se>".force_encoding("iso-8859-1"),
       },
 
       '=?ISO-8859-1?Q?Patrik_F=E4ltstr=F6m?= <paf / nada.kth.se>' => {
-        'iso-8859-1' => "Patrik F\xE4ltstr\xF6m <paf / nada.kth.se>",
+        'iso-8859-1' => "Patrik F\xE4ltstr\xF6m <paf / nada.kth.se>".force_encoding("iso-8859-1"),
       },
 
       'Nathaniel Borenstein <nsb / thumper.bellcore.com> (=?iso-8859-8?b?7eXs+SDv4SDp7Oj08A==?=)' => {
-        'iso-8859-8' => "Nathaniel Borenstein <nsb / thumper.bellcore.com> (\355\345\354\371 \357\341 \351\354\350\364\360)",
+        'iso-8859-8' => "Nathaniel Borenstein <nsb / thumper.bellcore.com> (\355\345\354\371 \357\341 \351\354\350\364\360)".force_encoding("iso-8859-8"),
         'utf-8'      => "Nathaniel Borenstein <nsb / thumper.bellcore.com> (\327\235\327\225\327\234\327\251 \327\237\327\221 \327\231\327\234\327\230\327\244\327\240)",
       },
 
